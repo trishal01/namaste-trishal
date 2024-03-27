8e3ca700280a5ef47bc9c11f19de0b91e4fba62f
Trishal <trishalwalia01@gmail.com>
2024-03-27 14:32:36 +0530

    Production ready application setup done

--- notes.js
+++ notes.js
@@ -6,4 +6,6 @@ Browsers list package is used to tell what level of browsers to support
 Transitive Dependency - 1 package dependa upon other packages and so on.
 
 Parcel package is super powerful and actually making th app faster
+
+npm start == npm run start (reserved) , npm build != npm run start
 */
